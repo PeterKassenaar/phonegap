a4aac1935a10e193332ba8b24b58a502f8b16944
Peter Kassenaar <info@kassenaar.com>
2016-11-23 13:45:50 +0100

    Fonts + verbeterde (?) movie en audio pagina

--- js/page5.js
+++ js/page5.js
@@ -1,17 +1,19 @@
 document.addEventListener('deviceready', onDeviceReady);
 
 function onDeviceReady() {
-	// Ga details ophalen uit de querystring
+	// Ga details ophalen uit de querystring, om te zien welke ID is meegegeven
 	var positie = window.location.search.indexOf('=');
 	var movieId = window.location.search.substring(positie+1);
 	var movieUrl = 'http://www.omdbapi.com/?i=';
 
+	// AJAX call om gegevens op te halen voor huidige movie (op basis van ID)
 	$.ajax({
 		url: movieUrl + movieId,
 		success: movieSuccess,
 		error: movieError
 	});
 
+	// Success handler, wordt aangeroepen bij return uit de API van movie database
 	function movieSuccess(movieData) {
 		$('#movieTitle').html(movieData.Title);
 		$('#movieYear').html(movieData.Year);
@@ -20,6 +22,7 @@ function onDeviceReady() {
 		$('#moviePlot').html(movieData.Plot);
 	}
 
+	// Error
 	function movieError(jqXhr, textStatus, errorThrown) {
 		alert('FOUT:' + textStatus);
 	}
